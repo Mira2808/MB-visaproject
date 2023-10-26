d3899d03e96ca1f90c2070d13f4aaec738329c1e
Mira Kikani <kikanimira288@gmail.com>
2023-10-26 21:15:31 +0530

    singapore

--- visaproject/visapp/urls.py
+++ visaproject/visapp/urls.py
@@ -5,4 +5,15 @@ from . import views
 urlpatterns = [
    path('index/',views.index,name='index'),
    path('registration/',views.registration,name='registration'),
+   path('service/',views.service,name='service'),
+   path('coaching/',views.coaching,name='coaching'),     
+   path('about/',views.about,name='about'), 
+   path('countries/',views.countries,name='countries'),
+   path('contact/',views.contact,name='contact'),
+   path('login/',views.login,name='login'),
+   path('ilets/',views.ilets,name='ilets'),
+   path('singapore/',views.singapore,name='singapore'),
+   
+   
+   
 ]
--- visaproject/visapp/views.py
+++ visaproject/visapp/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-     return render(request,"index.html")    
+     return render(request,"client/index.html")    
 
 def registration(request):
-     return render(request,"regis.html")       
+     return render(request,"client/regis.html") 
+
+def service(request):
+     return render(request,"client/service.html")
+
+def coaching(request):
+     return render(request,"client/coaching.html")
+
+def about(request):
+     return render(request,"client/about.html")  
+
+def countries(request):
+     return render(request,"client/countries.html")  
+
+def contact(request):
+     return render(request,"client/contact.html")  
+
+def login(request):
+     return render(request,"client/login.html") 
+
+def ilets(request):
+     return render(request,"client/ilets.html")
+
+def singapore(request):
+     return render(request,"client/singapore.html")
+
+ 
+
+
+   
